6b19002fccb26343ed3b59d99317f1157e423747
Zayar-Win <zayarwin7751@gmail.com>
2025-02-12 15:20:12 +0700

    done-tailwind-and-css-config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "uploadthing.com",
+      "utfs.io",
+      "img.clerk.com",
+      "subdomain",
+      "files.stripe.com",
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/routes/routes.ts
@@ -0,0 +1,5 @@
+const routes = {
+  SIGNIN: "/sign-in",
+};
+
+export default routes;
